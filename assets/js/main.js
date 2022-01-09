5ca96ddef453eb67876ef29e44848bba2ce5caad
mdtusharali <mdtusharali7400@gmail.com>
2022-01-09 21:05:04 +0600

    web portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function ($) {
+
+    $(".hero-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        autoplay: false
+    });
+
+
+
+
+});
